24ce2d7b370ebb8cd129596176642707d72f6b88
Steffen Beisenherz <s.beisenherz@kellerkinder.de>
2021-12-01 10:40:24 +0100

    Implement Day 1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,15 +1,37 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        var previousValue: Int? = null
+        var counter = 0
+
+        for (value in input) {
+            if (previousValue != null && previousValue < value.toInt()) counter++
+
+            previousValue = value.toInt()
+        }
+
+        return counter
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        var previousValue: Int? = null
+        var counter = 0
+
+        for (value in input.indices) {
+            if (input.lastIndex < value + 2) continue
+
+            val calculated = input[value].toInt() + input[value + 1].toInt() + input[value + 2].toInt()
+
+            if (previousValue != null && previousValue!! < calculated) counter++
+
+            previousValue = calculated
+        }
+
+        return counter
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 0)
 
     val input = readInput("Day01")
     println(part1(input))
